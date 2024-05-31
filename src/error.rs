51d0d18205b32473a19f615ab489698cdacc1aea
stossmt <stossmt@gmail.com>
2024-05-30 22:33:23 -0600

    Implement skelton of TrueType font parsing
    So far, parsed the tables discussed in this page of the spec: https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
    
    Remaining work is to improve error handling, parse the rest of the
    table, and handle some of the validation logic discussed in the spec.

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,22 @@ pub struct CapyError {
 
 #[derive(Debug)]
 pub enum ErrorCode {
-    BadArgument,
+    Cancelled,
+    Unknown,
+    InvalidArgument,
+    DeadlineExceeded,
+    NotFound,
+    AlreadyExists,
+    PermissionDenied,
+    ResourceExhausted,
+    FailedPrecondition,
+    Aborted,
+    OutOfRange,
+    Unimplemented,
+    Internal,
+    Unavailable,
+    DataLoss,
+    Unauthenticated,
 }
 
 impl std::error::Error for CapyError {
@@ -19,7 +34,22 @@ impl std::error::Error for CapyError {
 impl Display for ErrorCode {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            ErrorCode::BadArgument => write!(f, "[BAD ARGUMENT]"),
+            ErrorCode::Cancelled => write!(f, "CANCELLED"),
+            ErrorCode::Unknown => write!(f, "UNKNOWN"),
+            ErrorCode::InvalidArgument => write!(f, "INVALID_ARGUMENT"),
+            ErrorCode::DeadlineExceeded => write!(f, "DEADLINE_EXCEEDED"),
+            ErrorCode::NotFound => write!(f, "NOT_FOUND"),
+            ErrorCode::AlreadyExists => write!(f, "ALREADY_EXISTS"),
+            ErrorCode::PermissionDenied => write!(f, "PERMISSION_DENIED"),
+            ErrorCode::ResourceExhausted => write!(f, "RESOURCE_EXHAUSTED"),
+            ErrorCode::FailedPrecondition => write!(f, "FAILED_PRECONDITION"),
+            ErrorCode::Aborted => write!(f, "ABORTED"),
+            ErrorCode::OutOfRange => write!(f, "OUT_OF_RANGE"),
+            ErrorCode::Unimplemented => write!(f, "UNIMPLEMENTED"),
+            ErrorCode::Internal => write!(f, "INTERNAL"),
+            ErrorCode::Unavailable => write!(f, "UNAVAILABLE"),
+            ErrorCode::DataLoss => write!(f, "DATA_LOSS"),
+            ErrorCode::Unauthenticated => write!(f, "UNAUTHENTICATED"),
         }
     }
 }
@@ -27,7 +57,11 @@ impl Display for ErrorCode {
 impl Display for CapyError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         // FIXME: Print the entire error chain (the source field)
-        write!(f, "{} {}\n", self.error_impl.code, self.error_impl.message)?;
+        write!(
+            f,
+            "[{}] {}\n",
+            self.error_impl.code, self.error_impl.message
+        )?;
         if let Some(source) = &self.error_impl.source {
             write!(f, "   Caused by: {}\n", source)?;
         }
@@ -36,6 +70,16 @@ impl Display for CapyError {
 }
 
 impl CapyError {
+    pub fn new(code: ErrorCode, message: &'static str) -> Self {
+        Self {
+            error_impl: Box::new(ErrorImpl {
+                code,
+                message,
+                source: None,
+            }),
+        }
+    }
+
     pub fn with_source(
         code: ErrorCode,
         message: &'static str,
@@ -60,6 +104,10 @@ struct ErrorImpl {
 
 impl From<std::io::Error> for CapyError {
     fn from(err: std::io::Error) -> CapyError {
-        CapyError::with_source(ErrorCode::BadArgument, "I/O Error occurred", Box::new(err))
+        CapyError::with_source(
+            ErrorCode::InvalidArgument,
+            "I/O Error occurred",
+            Box::new(err),
+        )
     }
 }
--- src/font.rs
+++ src/font.rs
@@ -1,12 +1,199 @@
-use crate::error::CapyError;
+use crate::error::{CapyError, ErrorCode};
 use std::fmt::{self, Display, Formatter};
+use std::io::Read;
 
-pub struct Font {}
+pub struct Font {
+    // Required tables
+    font_directory_table: FontDirectoryTable,
+    cmap_table: CmapTable,
+    glyf_table: GlyfTable,
+    head_table: HeadTable,
+    hhea_table: HheaTable,
+    hmtx_table: HmtxTable,
+    loca_table: LocaTable,
+    maxp_table: MaxpTable,
+    name_table: NameTable,
+    post_table: PostTable,
+
+    // Optional tables
+    cvt_table: CvtTable,
+    fpgm_table: FpgmTable,
+    hdmx_table: HdmxTable,
+    kern_table: KernTable,
+    os2_table: Os2Table,
+    prep_table: PrepTable,
+}
+
+struct OffsetSubtable {
+    scalar_type: u32,
+    num_tables: u16,
+    search_range: u16,
+    entry_selector: u16,
+    range_shift: u16,
+}
+
+struct TableDirectorySubtable {
+    tag: u32,
+    check_sum: u32,
+    offset: u32,
+    length: u32,
+}
+
+struct FontDirectoryTable {
+    offset_subtable: OffsetSubtable,
+    table_directory_subtables: Vec<TableDirectorySubtable>,
+}
+
+struct CmapTable {}
+
+struct GlyfTable {}
+
+struct HeadTable {}
+
+struct HheaTable {}
+
+struct HmtxTable {}
+
+struct LocaTable {}
+
+struct MaxpTable {}
+
+struct NameTable {}
+
+struct PostTable {}
+
+struct CvtTable {}
+
+struct FpgmTable {}
+
+struct HdmxTable {}
+
+struct KernTable {}
+
+struct Os2Table {}
+
+struct PrepTable {}
 
 pub fn parse_from_file(filepath: &str) -> Result<Font, CapyError> {
-    let _message = std::fs::read_to_string(filepath)?;
-    // FIXME: implement actual font parsing
-    Ok(Font {})
+    let buffer = read_file_to_byte_buffer(filepath)?;
+    let mut parser = ByteParser::new(&buffer);
+
+    // Parse required tables
+    let font_directory_table = parse_font_directory_table(&mut parser)?;
+
+    Ok(Font {
+        // Required tables
+        font_directory_table,
+        cmap_table: CmapTable {},
+        glyf_table: GlyfTable {},
+        head_table: HeadTable {},
+        hhea_table: HheaTable {},
+        hmtx_table: HmtxTable {},
+        loca_table: LocaTable {},
+        maxp_table: MaxpTable {},
+        name_table: NameTable {},
+        post_table: PostTable {},
+
+        // Optional tables
+        cvt_table: CvtTable {},
+        fpgm_table: FpgmTable {},
+        hdmx_table: HdmxTable {},
+        kern_table: KernTable {},
+        os2_table: Os2Table {},
+        prep_table: PrepTable {},
+    })
+}
+
+struct ByteParser<'a> {
+    buffer: &'a [u8],
+    offset: usize,
+}
+
+impl<'a> ByteParser<'a> {
+    fn new(buffer: &'a [u8]) -> Self {
+        Self { buffer, offset: 0 }
+    }
+
+    fn read_be_u32(&mut self) -> Result<u32, CapyError> {
+        match self.buffer[self.offset..self.offset + 4].try_into() {
+            Ok(bytes) => {
+                let value = u32::from_be_bytes(bytes);
+                self.offset += 4;
+                Ok(value)
+            }
+            Err(_) => Err(CapyError::new(
+                ErrorCode::OutOfRange,
+                "Buffer too small for u32",
+            )),
+        }
+    }
+
+    fn read_be_u16(&mut self) -> Result<u16, CapyError> {
+        match self.buffer[self.offset..self.offset + 2].try_into() {
+            Ok(bytes) => {
+                let value = u16::from_be_bytes(bytes);
+                self.offset += 2;
+                Ok(value)
+            }
+            Err(_) => Err(CapyError::new(
+                ErrorCode::OutOfRange,
+                "Buffer too small for u32",
+            )),
+        }
+    }
+}
+
+fn parse_font_directory_table(parser: &mut ByteParser) -> Result<FontDirectoryTable, CapyError> {
+    let offset_subtable = parse_offset_table(parser)?;
+    let table_directory_subtables =
+        parse_table_directory_subtables(parser, offset_subtable.num_tables)?;
+    Ok(FontDirectoryTable {
+        offset_subtable,
+        table_directory_subtables,
+    })
+}
+
+fn parse_offset_table(parser: &mut ByteParser) -> Result<OffsetSubtable, CapyError> {
+    let scalar_type = parser.read_be_u32()?;
+    let num_tables = parser.read_be_u16()?;
+    let search_range = parser.read_be_u16()?;
+    let entry_selector = parser.read_be_u16()?;
+    let range_shift = parser.read_be_u16()?;
+
+    Ok(OffsetSubtable {
+        scalar_type,
+        num_tables,
+        search_range,
+        entry_selector,
+        range_shift,
+    })
+}
+
+fn parse_table_directory_subtables(
+    parser: &mut ByteParser,
+    num_tables: u16,
+) -> Result<Vec<TableDirectorySubtable>, CapyError> {
+    let mut subtables = Vec::new();
+    for _ in 0..num_tables {
+        let tag = parser.read_be_u32()?;
+        let check_sum = parser.read_be_u32()?;
+        let offset = parser.read_be_u32()?;
+        let length = parser.read_be_u32()?;
+        subtables.push(TableDirectorySubtable {
+            tag,
+            check_sum,
+            offset,
+            length,
+        });
+    }
+    Ok(subtables)
+}
+
+fn read_file_to_byte_buffer(filepath: &str) -> Result<Vec<u8>, CapyError> {
+    let mut file = std::fs::File::open(filepath)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
 }
 
 impl Display for Font {
